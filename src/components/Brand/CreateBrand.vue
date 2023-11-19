3d0d7d1598eb4bb5bb391944facb9e1e4eae3953
Saeid Doroudi <doroudi@outlook.com>
2023-11-19 23:24:53 +0330

    implement create brand add image selector

--- src/components/Brand/CreateBrand.vue
+++ src/components/Brand/CreateBrand.vue
@@ -1,34 +1,23 @@
 <script setup lang='ts'>
 import type { FormInst, FormRules } from 'naive-ui/es/form'
-import type { TreeSelectOption } from 'naive-ui/es/tree-select/src/interface'
 import { storeToRefs } from 'pinia'
-import type { Category, CategoryCreateModel } from '~/models/Category'
+import type { CategoryCreateModel } from '~/models/Category'
 
 const emits = defineEmits(['close'])
-const categoryStore = useCategoryStore()
-const { isLoading, categories } = storeToRefs(categoryStore)
-const categoryItem = ref<CategoryCreateModel>({ name: '', parentId: 0 })
+const brandStore = useBrandStore()
+const { isLoading } = storeToRefs(brandStore)
+const brandItem = ref<CategoryCreateModel>({ name: '', parentId: 0 })
 const { t } = useI18n()
 const formRef = ref<FormInst | null>(null)
 async function create() {
   formRef.value?.validate(async (errors: any) => {
     if (!errors) {
-      await categoryStore.createCategory(categoryItem.value)
+      await brandStore.createBrand(brandItem.value)
       emits('close')
     }
   })
 }
 
-const categoriesOptions = categories.value.map(mapCategoryToOptions)
-
-function mapCategoryToOptions(item: Category): TreeSelectOption {
-  return {
-    key: item.id,
-    label: item.name,
-    children: item.children?.map(mapCategoryToOptions),
-  }
-}
-const parents: TreeSelectOption[] = [{ key: 0, label: 'Root' }, ...categoriesOptions]
 const nameInput = ref()
 onMounted(() => {
   nameInput.value?.focus()
@@ -39,48 +28,74 @@ const rules: FormRules = {
     {
       required: true,
       trigger: ['blur', 'change'],
-      message: t('categories.validations.nameRequired'),
+      message: t('brands.validations.name'),
     },
   ],
-  parentId: [
+  url: [
     {
       required: true,
       trigger: ['blur', 'change'],
-      message: t('categories.validations.parentRequired'),
+      message: t('brands.validations.url'),
     },
   ],
+  image: [
+    {
+      required: true,
+      trigger: ['blur', 'change'],
+      message: t('brands.validations.image'),
+    },
+  ],
+}
 
+const showModalRef = ref(false)
+const previewImageUrlRef = ref('')
+function handlePreview(file: UploadFileInfo) {
+  const { url } = file
+  previewImageUrlRef.value = url as string
+  showModalRef.value = true
 }
 </script>
 
 <template>
-  <n-form ref="formRef" :model="categoryItem" :rules="rules" @submit.prevent="create()">
+  <n-form ref="formRef" :model="brandItem" :rules="rules" @submit.prevent="create()">
     <div class="form-control">
-      <n-form-item class="mb-5" path="name" :label="t('categories.create.categoryName')">
+      <n-form-item class="mb-5" path="name" :label="t('brands.create.brandName')">
         <n-input
-          id="name" ref="nameInput" v-model:value="categoryItem.name" autofocus
-          :placeholder="t('categories.create.categoryName')"
+          id="name" ref="nameInput" v-model:value="brandItem.name" autofocus
+          :placeholder="t('brands.create.brandName')"
         />
       </n-form-item>
     </div>
-    <div class="form-control">
-      <n-form-item class="mb-5" path="name" :label="t('categories.create.categoryName')">
+    <div class="form-control flex flex-col mb-5">
+      <n-form-item path="url" :label="t('brands.create.url')">
         <n-input
-          id="name" ref="nameInput" v-model:value="categoryItem.name" autofocus
-          :placeholder="t('categories.create.categoryName')"
+          v-model:value="brandItem.url" autofocus
+          :placeholder="t('brands.create.url')"
         />
       </n-form-item>
     </div>
+
     <div class="form-control">
-      <n-form-item class="mb-5" :label="t('categories.create.parent')">
-        <n-tree-select
-          v-model="categoryItem.parentId" key-field="key" :options="parents"
-          :placeholder="t('categories.create.parent')" default-value="Root"
+      <n-form-item class="mb-5" path="image" :label="t('brands.create.image')">
+        <n-upload
+          :default-file-list="previewFileList"
+          list-type="image-card"
+          accept="image/png, image/jpeg"
+          max="1"
+          @preview="handlePreview"
         />
+        <n-modal
+          v-model:show="showModal"
+          preset="card"
+          style="width: 600px"
+          title="A Cool Picture"
+        >
+          <img :src="previewImageUrl" style="width: 100%">
+        </n-modal>
       </n-form-item>
     </div>
     <n-button attr-type="submit" size="large" :block="true" type="primary" :loading="isLoading">
-      {{ t('categories.create.buttonTitle') }}
+      {{ t('brands.create.buttonTitle') }}
     </n-button>
   </n-form>
 </template>
