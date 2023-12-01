b294909954b3eeb9be7d483725e9d24579295dda
Saeid Doroudi <doroudi@outlook.com>
2023-12-01 10:58:16 +0330

    feat: :lipstick: add specific favicon Fixes #36

--- vite.config.ts
+++ vite.config.ts
@@ -87,27 +87,16 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+      includeAssets: ['favicon.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
-        theme_color: '#ffffff',
+        name: 'YummyAdmin',
+        short_name: 'YummyAdmin',
+        theme_color: '#459858',
         icons: [
           {
             src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
           },
         ],
       },
