5ee55579cc1c8e6023caf88fe23cf8297c550acf
Saeid Doroudi <doroudi@outlook.com>
2024-04-28 14:29:16 +0330

    fix linting issue

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -1,17 +1,9 @@
 import type { AppModule } from '~/types'
 
 export const install: AppModule = (app, router) => {
-  console.log(router)
   router.isReady()
     .then(async () => {
       const { registerSW } = await import('virtual:pwa-register')
       registerSW({ immediate: true })
     }).catch(() => { })
-
-  // registerSW({
-  //   onNeedRefresh() {
-  //     // useNotificationsStore().addNotification({ message: 'New Version available', permanent: true })
-  //   },
-  //   onOfflineReady() { },
-  // })
 }
