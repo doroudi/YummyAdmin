639e0cb55d1aaa44797236fe70601fb3f11b2f34
Saeid Doroudi <doroudi@outlook.com>
2024-11-05 22:14:48 +0330

    :package: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -28,9 +28,15 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
 
   plugins: [
-    // basicSsl(),
     VueMacros({
       plugins: {
         vue: Vue({
