0344e94efed4e1807920fc32b15f6b76fbb4b469
Saeid Doroudi <doroudi@outlook.com>
2024-11-22 22:23:30 +0330

    :package: update msw

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,21 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
 import { createRouter, createWebHistory } from 'vue-router'
 
-// import { routes } from 'vue-router/auto-routes'
 import type { AppModule } from './types'
 import App from './App.vue'
-import i18n from './modules/i18n'
+
 import '@unocss/reset/tailwind-compat.css'
 import 'uno.css'
 import './styles/main.scss'
 
 declare module '@vue/runtime-core' {
+
   export interface ComponentCustomProperties {
     $filters: any
   }
 }
 
 async function enableMocking() {
-  // const isMocking = import.meta.env.VITE_API_MOCKING_ENABLED
-  // if (!isMocking)
-  //   return Promise.resolve()
   const { worker } = await import('~/mocks/browser')
   return worker.start({
     onUnhandledRequest: 'bypass',
@@ -32,7 +29,6 @@ const router = createRouter({
   routes,
 })
 
-// TODO: try to use Vitessg instead of ClientSide vue app
 const app = createApp(App)
 app.use(router)
 Object.values(import.meta.glob<{ install: AppModule }>('./modules/*.ts', { eager: true }))
@@ -44,18 +40,11 @@ Object.values(import.meta.glob<any>('./common/filters/*.filter.ts', { eager: tru
   .forEach(filters => Object.keys(filters).forEach(func => app.config.globalProperties.$filters[func] = filters[func]))
 
 router.beforeEach((to, from, next) => {
-  // @ts-expect-error "Type instantiation is excessively deep and possibly infinite.ts(2589)"
-  const { t } = i18n.global
-  let title = t('title')
+  let title = 'Yummy Admin'
   if (to.meta.title)
     title = `${to.meta.title} - ${title}`
 
   document.title = title
-
-  // const isAuthenticated = useAccountStore().isAuthenticated()
-  // const isAuthRequired = to.meta.authRequired ?? true
-  // if (isAuthRequired && !isAuthenticated)
-  //   next({ path: '/account/login' })
 
   next()
 })
