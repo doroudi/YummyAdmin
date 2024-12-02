2a4a3ac4b628f382b257bb2bdcd127322bd33ec4
Saeid Doroudi <doroudi@outlook.com>
2024-12-02 07:11:24 +0330

    reorganize nav bar add theme customize component

---
+++ src/components/Navbar/ThemeCustomize.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+import {
+    Settings48Regular as CustomizeIcon,
+} from '@vicons/fluent'
+
+const layout = useLayoutStore()
+</script>
+
+<template>
+    <div v-bind="$attrs">
+        <n-button quaternary circle @click="layout.toggleTheme()">
+            <template #icon>
+                <NIcon size="1.4rem">
+                    <CustomizeIcon class="rotate-animated" />
+                </NIcon>
+            </template>
+        </n-button>
+    </div>
+</template>
+
+<style scoped lang='scss'></style>
