a22e22f812372dc225f7490b5b52b03ff08649de
Saeid Doroudi <doroudi@outlook.com>
2024-12-04 21:37:23 +0330

    add theme customize component

---
+++ src/components/CustomizeDialog/CustomizeDialog.vue
@@ -0,0 +1,73 @@
+<script setup>
+import {
+  WeatherMoon48Regular as MoonIcon,
+  WeatherSunny48Regular as SunIcon,
+} from '@vicons/fluent'
+
+const { t } = useI18n()
+const theme = ref('light')
+const layout = useLayoutStore()
+function setLight() {
+  if (layout.isDark)
+    layout.toggleTheme()
+}
+
+function setDark() {
+  if (!layout.isDark)
+    layout.toggleTheme()
+}
+</script>
+
+<template>
+  <div class="section">
+    <NTag type="primary" :bordered="false" size="small" class="mb-3 font-bold">
+      {{ t('customize.theme') }}
+    </NTag>
+
+    <n-space justify="center" size="large">
+      <NButton
+        ghost class="p-7" :type="layout.isDark === false ? 'primary' : 'default'"
+        size="large" @click="setLight"
+      >
+        <template #icon>
+          <NIcon>
+            <SunIcon />
+          </NIcon>
+        </template>
+      </NButton>
+
+      <NButton
+        ghost class="w-full p-7" :type="layout.isDark ? 'primary' : 'default'" size="large"
+        @click="setDark"
+      >
+        <template #icon>
+          <NIcon>
+            <MoonIcon />
+          </NIcon>
+        </template>
+      </NButton>
+    </n-space>
+  </div>
+  <div class="section">
+    <NTag type="primary" :bordered="false" size="small" class="mb-3 font-bold">
+      {{ t('customize.color') }}
+    </NTag>
+  </div>
+  <div class="section">
+    <NTag type="primary" :bordered="false" size="small" class="mb-3 font-bold">
+      {{ t('customize.layout') }}
+    </NTag>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.section {
+    margin-bottom: 1rem;
+    .section-title {
+        font-weight: 500;
+        padding: 0.2rem 0;
+        border-bottom: solid 1px #CCC;
+        margin-bottom:1rem;
+    }
+}
+</style>
--- src/components/Navbar/ThemeCustomize.vue
+++ src/components/Navbar/ThemeCustomize.vue
@@ -1,21 +1,32 @@
 <script setup lang='ts'>
 import {
-    Settings48Regular as CustomizeIcon,
+  Settings48Regular as CustomizeIcon,
 } from '@vicons/fluent'
 
-const layout = useLayoutStore()
+const showCustomizeDialog = ref(false)
+const { t } = useI18n()
+
+function toggleDialog() {
+  showCustomizeDialog.value = !showCustomizeDialog.value
+}
 </script>
 
 <template>
-    <div v-bind="$attrs">
-        <n-button quaternary circle @click="layout.toggleTheme()">
-            <template #icon>
-                <NIcon size="1.4rem">
-                    <CustomizeIcon class="rotate-animated" />
-                </NIcon>
-            </template>
-        </n-button>
-    </div>
+  <div v-bind="$attrs">
+    <n-button quaternary circle @click="toggleDialog()">
+      <template #icon>
+        <NIcon size="1.4rem">
+          <CustomizeIcon class="rotate-animated" />
+        </NIcon>
+      </template>
+    </n-button>
+  </div>
+
+  <n-drawer v-model:show="showCustomizeDialog" :width="400" placement="right">
+    <n-drawer-content closable :title="t('customize.title')">
+      <CustomizeDialog @close="showCustomizeDialog = false" />
+    </n-drawer-content>
+  </n-drawer>
 </template>
 
 <style scoped lang='scss'></style>
