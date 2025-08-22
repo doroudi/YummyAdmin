533dc234283af9e2ed5fd5c934cd43f8fb6a4e37
Saeid Doroudi <doroudi@outlook.com>
2025-08-22 20:45:42 +0330

    chore: add support links

--- src/components/shared/SupportProject.vue
+++ src/components/shared/SupportProject.vue
@@ -11,7 +11,7 @@ async function getGithubStarsCount() {
 
 <template>
   <div class="support-button ltr font-">
-    <button href="#"
+    <button href="https://github.com/doroudi/yummyadmin" target="_blank"
       class="github-button flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2">
       <span
         class="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
